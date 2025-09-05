b41c6e776cd72c86fc14a8dc4660e1a3fa91e435
Ighsaan <ighsaan.gamieldien@gmail.com>
2025-09-05 19:04:40 +0200

    Implemented the collect() method which returns a Collection of Integers from a String of comma delimited numbers.

--- src/main/java/numberrangesummarizer/NumberRangeSummarizerImpl.java
+++ src/main/java/numberrangesummarizer/NumberRangeSummarizerImpl.java
@@ -1,12 +1,21 @@
 package numberrangesummarizer;
 
+import java.util.Arrays;
 import java.util.Collection;
-import java.util.List;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
 
 public class NumberRangeSummarizerImpl implements NumberRangeSummarizer{
+
     @Override
     public Collection<Integer> collect(String input) {
-        return List.of();
+        String[] inputArray = input.split(",");
+
+        Collection<Integer> collection = Arrays.stream(inputArray)
+                .map(Integer::parseInt)
+                .collect(Collectors.toCollection(ArrayList::new));
+
+        return collection;
     }
 
     @Override
