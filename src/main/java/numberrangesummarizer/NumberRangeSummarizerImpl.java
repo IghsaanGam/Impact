7479953ac5123572d7fee0aa3d95bc4048164f6e
Ighsaan <ighsaan.gamieldien@gmail.com>
2025-09-05 17:48:31 +0200

    Added default implementation of NumberRangeSummarizer in NumberRangeSummarizerImpl and overrode its methods.

---
+++ src/main/java/numberrangesummarizer/NumberRangeSummarizerImpl.java
@@ -0,0 +1,16 @@
+package numberrangesummarizer;
+
+import java.util.Collection;
+import java.util.List;
+
+public class NumberRangeSummarizerImpl implements NumberRangeSummarizer{
+    @Override
+    public Collection<Integer> collect(String input) {
+        return List.of();
+    }
+
+    @Override
+    public String summarizeCollection(Collection<Integer> input) {
+        return "";
+    }
+}
