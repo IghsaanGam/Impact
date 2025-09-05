3d2176651356d8bcc26dc53121c83e2e5e2a5be6
Ighsaan <ighsaan.gamieldien@gmail.com>
2025-09-06 01:40:59 +0200

    Main method that displays an implementation of the NumberRangeSummarizer using a sample comma delimited list.

--- src/main/java/org/impact/Main.java
+++ src/main/java/org/impact/Main.java
@@ -1,9 +1,24 @@
 package org.impact;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import numberrangesummarizer.NumberRangeSummarizer;
+import numberrangesummarizer.NumberRangeSummarizerImpl;
+
+import java.util.Collection;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+        NumberRangeSummarizer summarizer= new NumberRangeSummarizerImpl();
+
+        String list = "1,3,6,7,8,12,13,14,15,21,22,23,24,31";
+
+        Collection<Integer> collection = summarizer.collect(list);
+
+        System.out.println(collection);
+
+        String summarizedCollection = summarizer.summarizeCollection(collection);
+
+        System.out.println(summarizedCollection);
+
     }
 }
