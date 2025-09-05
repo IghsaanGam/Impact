4a78a04350260e049c6a4f202b4d4651c7744ccd
Ighsaan <ighsaan.gamieldien@gmail.com>
2025-09-06 01:29:21 +0200

    Implemented the summarizeCollection() method which returns comma delimited list of numbers, as a String, groups numbers into a range when they are sequential. Added an isSequential method that checks if the second parameter comes after the number in the first parameter.

--- src/main/java/numberrangesummarizer/NumberRangeSummarizerImpl.java
+++ src/main/java/numberrangesummarizer/NumberRangeSummarizerImpl.java
@@ -20,6 +20,56 @@ public class NumberRangeSummarizerImpl implements NumberRangeSummarizer{
 
     @Override
     public String summarizeCollection(Collection<Integer> input) {
-        return "";
+        int size = input.size();
+        int counter=0;
+        ArrayList<Integer> inputArray = new ArrayList<>(input);
+        String range ="";
+        StringBuilder result = new StringBuilder();
+
+        while(counter<size){
+            int currentNumber = inputArray.get(counter);
+            int nextNumber = inputArray.get(counter+1);
+
+            boolean isSequential = isSequential(currentNumber, nextNumber);
+
+            if(isSequential && range.isEmpty()){
+                range = currentNumber + "-" + nextNumber;
+                counter++;
+                if(counter==size-1){
+                    result.append(range);
+                    break;
+                }
+            }
+            else if(isSequential && !range.isEmpty()){
+                range = range.split("-")[0] + "-" + nextNumber;
+                counter++;
+                if(counter==size-1){
+                    result.append(range);
+                    break;
+                }
+            }
+            else if(!isSequential && range.isEmpty()){
+                counter++;
+                if(counter==size-1){
+                    result.append(currentNumber).append(", ").append(nextNumber);
+                    break;
+                }
+                result.append(currentNumber).append(", ");
+            }
+            else if(!isSequential && !range.isEmpty()){
+                counter++;
+                if(counter==size-1){
+                    result.append(range).append(", ").append(nextNumber);
+                    break;
+                }
+                result.append(range).append(", ");
+                range="";
+            }
+        }
+        return result.toString();
+    }
+
+    private boolean isSequential(int currentNumber, int nextNumber){
+        return currentNumber+1==nextNumber;
     }
 }
