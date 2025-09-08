50d9a3c0a4d906cf854eeaee1cc84e3ce94f251e
Ighsaan <ighsaan.gamieldien@gmail.com>
2025-09-09 01:00:09 +0200

    Added tests that tests the exceptions to be thrown by giving incorrect input.

--- src/test/java/numberrangesummarizer/NumberRangeSummarizerImplTest.java
+++ src/test/java/numberrangesummarizer/NumberRangeSummarizerImplTest.java
@@ -17,10 +17,26 @@ public class NumberRangeSummarizerImplTest {
     public void stringOfOnlyCommaDelimitedNumbersShouldReturnCollectionOfThoseNumbersAsIntegers(){
         String list = "1,3,6,7,8,12,13,14,15,21,22,23,24,31";
         Collection<Integer> integerCollection = numberRangeSummarizer.collect(list);
-
         Assertions.assertEquals(14, numberRangeSummarizer.collect(list).size());
         Assertions.assertEquals("[1, 3, 6, 7, 8, 12, 13, 14, 15, 21, 22, 23, 24, 31]", integerCollection.toString());
         Assertions.assertFalse(integerCollection.isEmpty());
+    }
+
+    @Test
+    @DisplayName("Throws an IllegalArgumentException if list contains any non numerical characters")
+    public void stringOfListContainingCommaDelimitedCharactersAndStringsShouldThrowIllegalArgumentException(){
+        String list = "1,3,6,7,8,12,13,14,15,21,22,23,24,31,a,b,qweruiwqeyr1312";
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            numberRangeSummarizer.collect(list);
+        });
+    }
+
+    @Test
+    public void numberFromStringIsTooLarge(){
+        String list = "11111111111111111111111111";
+        Assertions.assertThrows(NumberFormatException.class, () -> {
+            numberRangeSummarizer.collect(list);
+        });
     }
 
 }
