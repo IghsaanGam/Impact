b3fd735755bb54f21ef6ca74553cec77e7146d4c
Ighsaan <ighsaan.gamieldien@gmail.com>
2025-09-09 09:19:58 +0200

    Changed display names of tests

--- src/test/java/numberrangesummarizer/NumberRangeSummarizerImplTest.java
+++ src/test/java/numberrangesummarizer/NumberRangeSummarizerImplTest.java
@@ -14,7 +14,7 @@ public class NumberRangeSummarizerImplTest {
     }
 
     @Test
-    @DisplayName("Returns a collection of integers, given a string of comma delimited integers")
+    @DisplayName("Given a string of comma delimited integers, returns a collection of those integers")
     public void stringOfOnlyCommaDelimitedNumbersShouldReturnCollectionOfThoseNumbersAsIntegers(){
         String list = "1,3,6,7,8,12,13,14,15,21,22,23,24,31";
         Collection<Integer> integerCollection = numberRangeSummarizer.collect(list);
@@ -37,16 +37,16 @@ public class NumberRangeSummarizerImplTest {
     }
 
     @Test
-    @DisplayName("Throws a NumberFormatException if list contains any number larger than an integer")
+    @DisplayName("Throws a IllegalArgumentException if list contains any number bigger than an integer")
     public void numberFromStringIsTooLarge(){
-        String list = "11111111111111111111111111";
-        Assertions.assertThrows(NumberFormatException.class, () -> {
+        String list = "11111111111111111111111111,1";
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
             numberRangeSummarizer.collect(list);
         });
     }
 
     @Test
-    @DisplayName("Returns a summarized comma delimited list of numbers as a string with sequential numbers being grouped in ranges")
+    @DisplayName("Given a collection of integers, returns a summarized comma delimited list of numbers as a string with sequential numbers being grouped in ranges")
     public void givenACollectionOfIntegersCreateACommaDelimitedListOfNumbersAsAStringWithConsecutiveNumbersBeingGroupedInARange(){
         String list = "1,3,6,7,8,12,13,14,15,21,22,23,24,31";
         Collection<Integer> integerCollection = numberRangeSummarizer.collect(list);
