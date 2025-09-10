388ed4191e3d8367b3c27e6fa57d18538e2f4a7e
Ighsaan <ighsaan.gamieldien@gmail.com>
2025-09-10 17:11:29 +0200

    summarizeCollection() throws illegal argument exception if input is null

--- src/main/java/numberrangesummarizer/NumberRangeSummarizerImpl.java
+++ src/main/java/numberrangesummarizer/NumberRangeSummarizerImpl.java
@@ -9,6 +9,9 @@ public class NumberRangeSummarizerImpl implements NumberRangeSummarizer{
 
     @Override
     public Collection<Integer> collect(String input) {
+        if(input==null || input.isEmpty()){
+            throw new IllegalArgumentException("Input string cannot be null nor empty");
+        }
         String[] inputArray = input.split(",");
 
         for(String item : inputArray){
@@ -28,6 +31,9 @@ public class NumberRangeSummarizerImpl implements NumberRangeSummarizer{
 
     @Override
     public String summarizeCollection(Collection<Integer> input) {
+        if(input==null){
+            throw new IllegalArgumentException("Input cannot be null");
+        }
         int size = input.size();
         int counter=0;
         ArrayList<Integer> inputArray = new ArrayList<>(input);
